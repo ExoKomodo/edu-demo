f637038ddf294c7cd6300687a6d0393e6d9c2fff
James Orson <jamesaorson@gmail.com>
2023-06-23 14:01:33 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+echo 'Setting up local environment for Edu Demo'
+
+# NOTE: Add tool installations and such here. Say you need docker-compose to run the application locally, then this should install that for you.
--- tracking/0-laying-a-foundation/1-create-starting-scripts.sh
+++ tracking/0-laying-a-foundation/1-create-starting-scripts.sh
@@ -18,5 +18,5 @@ touch ./setup.sh
 echo "#! /bin/bash
 echo 'Setting up local environment for Edu Demo'
 
-# NOTE: Add tool installations and such here. Say you need docker-compose to run the application locally, then this should install that for you.
-" >> ./setup.sh
+# NOTE: Add tool installations and such here. Say you need docker-compose to run the application locally, then this should install that for you." \
+    >> ./setup.sh
